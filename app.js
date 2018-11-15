102b0929568fef9cd6045524dd659a99cad79953
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2018-11-15 14:20:26 -0800

    responded class functionality

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 const form = document.getElementById('registrar');
 const input = form.querySelector('input');
+const ul = document.getElementById('invitedList');
+
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const inputText = input.value;
-  const ul = document.getElementById('invitedList');
   const li = document.createElement('li');
+  const inputText = input.value;
+  const label = document.createElement('label');
+  const checkBox = document.createElement('input');
+
   li.textContent = inputText;
+  label.textContent = 'Confirmed';
+  checkBox.type = 'checkBox';
+  label.appendChild(checkBox);
+  li.appendChild(label);
   ul.appendChild(li);
   input.value = '';
+})
+
+ul.addEventListener('change', (e) => {
+  const checkbox = event.target;
+  const checked = checkbox.checked;
+  const listItem = checkbox.parentNode.parentNode;
+
+  if(checked) {
+    listItem.className = 'responded';
+  } else {
+    listItem.className = '';
+  }
 })
